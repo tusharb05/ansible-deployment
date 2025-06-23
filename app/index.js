3a27661ed71aff2618bd5cd3fda80d13f904b606
tusharb05 <tusharbaweja2005@gmail.com>
2025-06-23 18:45:30 +0000

    minor change

--- app/index.js
+++ app/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const ip = require("ip");
 
 const app = express();
 
 app.get("/", (req, res) => {
 	res.json({
 		msg: `i am healthy - ${process.env.SERVER_NAME || "no-name"}`,
-		ip: ip.address(),
 	});
 });
 
 app.listen(3000, () => {
 	console.log(
-		`server (${process.env.SERVER_NAME}) running on ip: `,
-		ip.address()
+		`server (${process.env.SERVER_NAME}) running on ip: `
 	);
 });
